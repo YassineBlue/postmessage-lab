176790d30e7b3ace333e5822f5208e5d22077ce7
YassineBlue <yassineyassar1@gmail.com>
2026-05-20 02:22:22 +0100

    Refactor message sending and receiving in parent.js
    Refactor message handling and update trusted origin to use window location.

--- js/parent.js
+++ js/parent.js
@@ -1,16 +1,24 @@
 const chatBox = document.getElementById('chat-box');
 const iframe = document.getElementById('child-iframe');
-const trustedOrigin = 'http://127.0.0.1'; 
-
+// const trustedOrigin = 'http://127.0.0.1'; // use this one for local hosting 
+const trustedOrigin = window.location.origin;
 // 1. Sending to Child
 function sendMessageToChild() {
     const input = document.getElementById('parent-input');
     const msg = input.value;
     
+    //avoiding empty inputs
+    if (!msg.trim()) return;
     // Target the iframe's content window
     iframe.contentWindow.postMessage(msg, trustedOrigin);
-    
-    chatBox.innerHTML += `<div><strong>You (Parent):</strong> ${msg}</div>`;
+
+    const div = document.createElement('div');
+    const str = document.createElement('strong');
+    str.textContent = 'You (Parent): ';
+    const text = document.createTextNode(msg); 
+    str.appendChild(text);
+    div.appendChild(str);
+    chatBox.appendChild(div);
     input.value = '';
 }
 
@@ -18,5 +26,12 @@ function sendMessageToChild() {
 window.addEventListener('message', (event) => {
     if (event.origin !== trustedOrigin) return; // Security filter
 
-    chatBox.innerHTML += `<div style="color: blue;"><strong>Child Node:</strong> ${event.data}</div>`;
+    const div = document.createElement('div');
+    div.style.color = 'blue';
+    const str = document.createElement('strong');
+    str.textContent = 'From child: ';
+    const txt = document.createTextNode(event.data); 
+    div.appendChild(str);
+    div.appendChild(txt);
+    chatBox.appendChild(div);
 });
